2f05ea3cba5f79109a5deed8bda13d4055c215fe
Akhil <akhilsringeri123@gmail.com>
2025-08-07 13:08:35 +0530

    feat: Update CI/CD workflow and enhance user data roles

--- src/constants/sampleData.js
+++ src/constants/sampleData.js
@@ -1,19 +1,19 @@
 const userData = [
   {
     name: 'Akhil',
-    role: 'Software Dev',
+    role: 'Software Developer',
     description: 'Software engineer with 2.6 years of Experience',
     imageSrc: 'https://res.cloudinary.com/akhilsrg/image/upload/v1751467709/gc0oq8pq8s4way76irbc.jpg'
   },
   {
     name: 'Anish',
-    role: 'QA',
+    role: 'QA Engineer',
     description: 'Automation tester with 2.6 years of Experience',
     imageSrc: 'https://res.cloudinary.com/akhilsrg/image/upload/v1751467750/x8w3mzqo9uxefl28tjva.jpg'
   },
   {
-    name: 'Sanjay',
-    role: 'DevOps',
+    name: 'Avinash',
+    role: 'DevOps Engineer',
     description: 'DevOps engineer with 3 years of Experience. Expert in CI/CD pipelines and cloud services.',
     imageSrc: 'https://res.cloudinary.com/akhilsrg/image/upload/v1751479894/lpzjrse8bszf6sfrgscw.jpg'
   }
